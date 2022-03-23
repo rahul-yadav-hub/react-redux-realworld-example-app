a6c128fbea3afb8d3ab5e41d7081487c2a16c04d
Rahul Yadav <55504874+rahul-yadav-hub@users.noreply.github.com>
2022-03-23 19:24:46 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://rahul-mern-lb-d699e4cfaec1164c.elb.ap-south-1.amazonaws.com:3000/api';
+const API_ROOT = 'https://rahul-wp.squareops.co.in/';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
